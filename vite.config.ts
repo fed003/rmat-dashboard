1e6aa6882a2c8623395dda871f58f348bc7554c5
Alex Federici <fed003@gmail.com>
2025-02-25 16:50:38 -0800

    Add TypeScript support with tsconfig and vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,7 @@
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 
-// https://vite.dev/config/
 export default defineConfig({
-	base: "/rmat-dashboard/",
 	plugins: [vue()],
+	base: "/rmat-dashboard/", // Already set for GitHub Pages
 });
